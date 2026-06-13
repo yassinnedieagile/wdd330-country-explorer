ef8dd3fb1fd0dea0d1822e32de1f7a74068b1da5
Nedie Yassin <yassinnedie9121@gmail.com>
2026-06-14 01:25:04 +0200

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {resolve} from "path";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+
+    root: "src/",
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                country: resolve(__dirname, "src/country.html"),
+            },
+        },
+    },
 })
